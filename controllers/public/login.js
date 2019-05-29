16cca23edacad75d8e284a43c772794fc4702da7
樊迪 <214333890@qq.com>
2019-05-29 18:21:14 +0800

    稍作修改

---
+++ controllers/public/login.js
@@ -0,0 +1,8 @@
+const request = require('request');
+let main = {
+    post: function(req, res, next) {
+
+    }
+}
+
+module.exports = main;
--- routes/public.js
+++ routes/public.js
@@ -2,34 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test', function (req, res, next) {
-  g_mysql.Query('selesct * from fd', '', function (result) {
-    if(result.code === -1)
-      return res.fd.fail(result.msg);
-    res.fd.success(JSON.stringify(result.msg.results));
-  })
-});
-
-router.get('/test1', async function (req, res, next) {
-  let result = await g_mysql.QuerySync('selsect * from fd', '');
-  if (result.code === -1)
-    return res.fd.fail(result.msg);
-  res.fd.success(JSON.stringify(result.results));
-});
-
-router.get('/test2', function (req, res, next) {
-  g_redis.Query('get1', 'fd', function (result) {
-    if(result.code === -1) 
-      return res.fd.fail(result.msg);
-    res.fd.success(result.msg);
-  })
-});
-
-router.get('/test3', async function (req, res, next) {
-  let result = await g_redis.QuerySync('get1', 'fd');
-  if(result.code === -1) 
-      return res.fd.fail(result.msg);
-  res.fd.success(result.msg);
+router.post('/login', function (req, res, next) {
+  let func = require('../controllers/public/login').post;
+  func(req, res, next);
 });
 
 
